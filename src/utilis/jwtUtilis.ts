ad10375f00fca5b412c155190a2704876ffea965
draskonovakovic <116906185+draskonovakovic@users.noreply.github.com>
2024-12-11 08:54:15 +0100

    event notifications logic refactor

--- src/utilis/jwtUtilis.ts
+++ src/utilis/jwtUtilis.ts
@@ -10,4 +10,13 @@ export const jwtUtils = {
   verifyToken(token: string): any {
     return jwt.verify(token, SECRET_KEY);
   },
+
+  extractToken(authHeader: string | undefined): string | null {
+    if (!authHeader) return null;
+    const parts = authHeader.split(' ');
+    if (parts.length === 2 && parts[0] === 'Bearer') {
+      return parts[1];
+    }
+    return null;
+  }
 };
