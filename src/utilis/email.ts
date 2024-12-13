18bd33f80a05cf6e9e4f22570492a48c553b5d93
draskonovakovic <116906185+draskonovakovic@users.noreply.github.com>
2024-12-13 10:27:46 +0100

    invitation controller fix

--- src/utilis/email.ts
+++ src/utilis/email.ts
@@ -4,8 +4,8 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.ethereal.email',
   port: 587,
   auth: {
-      user: 'annabell.wuckert94@ethereal.email',
-      pass: 'vkh3uKrjdYXpek45p4'
+      user: 'haylie35@ethereal.email',
+      pass: '33rfWydYp72Trjxm8y'
   }
 });
 
