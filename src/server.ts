d17e3dd2c98a732e4c5d9f3796eb3b7f7a91e4bf
draskonovakovic <116906185+draskonovakovic@users.noreply.github.com>
2024-12-13 15:49:38 +0100

    Feat/event notifications (#6)
    * login and logout logic
    
    * events crud funcionality
    
    * authentication check added
    
    * real time events overview via websocket
    
    * events model updated with type field
    
    * more error handling
    
    * more error handling and event validation added
    
    * event types global constant and unknown error handling
    
    * cancel event functions with error handling
    
    * small event repository refactor
    
    * early return and canceled event id returned in response body
    
    * filter data and search
    
    * is active field filter
    
    * authentication added to event routes
    
    * filter function in repo refactored
    
    * updated event message via websocket
    
    * problem with time zone solved
    
    * update notification and event reminder notification
    
    * event notifications logic refactor
    
    * event filtering repository function fixed
    
    * missed comment solved
    
    * filter refactored

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,11 @@
-import http from 'http';  
+import http from 'http';
 import app from './app';
-import { Server as SocketIOServer } from 'socket.io';  
-import { config } from './config/config'; 
-import { jwtUtils } from './utilis/jwtUtilis';
+import { initializeSocketIO } from './socket';
+import { config } from './config/config';
 
 const server = http.createServer(app);
 
-export const io = new SocketIOServer(server, {
-  cors: {
-    origin: 'http://localhost:3000', 
-    methods: ['GET', 'POST'],        
-    credentials: true,               
-  },
-});
-
-
-io.use((socket, next) => {
-  const authHeader = socket.handshake.headers.authorization; 
-  const token = authHeader?.split(' ')[1]; 
-
-  if (!token) {
-    console.error('No token provided');
-    return next(new Error('Authentication error'));
-  }
-
-  try {
-    const user = jwtUtils.verifyToken(token); 
-    socket.data.user = user; 
-    next();
-  } catch (error: any) {
-    console.error('Token verification failed:', error.message);
-    next(new Error('Authentication error'));
-  }
-});
-
-io.on('connection', (socket) => {
-  console.log('User connected:', socket.data.user);
-});
+export const io = initializeSocketIO(server);
 
 server.listen(config.server.port, () => {
   console.log(`Server running on port ${config.server.port}`);
